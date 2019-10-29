e89114230ff1b2d5c18db3f6b7db3dc9af487f89
Simon Acevedo Rodriguez <sacevedo@bu.edu>
2019-10-28 20:33:28 -0400

    initial commit

---
+++ app/src/main/java/com/example/marketplace/NavBarFragment.java
@@ -0,0 +1,22 @@
+package com.example.marketplace;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class NavBarFragment extends Fragment {
+
+    public NavBarFragment(){
+        //Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_nav_bar, container, false);
+        return view;
+    }
+}
