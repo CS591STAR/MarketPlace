910f735c3ddbdb0ebeda4ac5738d60f4587c5308
mirtim <mirtim@bu.edu>
2019-11-03 00:22:26 -0400

    bar fragment

--- app/src/main/java/com/example/marketplace/MainActivity.java
+++ app/src/main/java/com/example/marketplace/MainActivity.java
@@ -9,16 +9,16 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button loginBtn;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loginBtn = findViewById(R.id.loginBtn);
+        btnLogin = findViewById(R.id.btnLogin);
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), MarketFeed.class);
--- app/src/main/java/com/example/marketplace/NavBarFragment.java
+++ app/src/main/java/com/example/marketplace/NavBarFragment.java
@@ -4,10 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class NavBarFragment extends Fragment {
+
+    private Button btnProfile;
+    private Button btnChat;
+    private TextView txtSearch;
+    private Button btnSearch;
+    private Button btnSell;
 
     public NavBarFragment(){
         //Required empty public constructor
@@ -17,6 +25,14 @@ public class NavBarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_nav_bar, container, false);
+
+        btnProfile = view.findViewById(R.id.btnProfile);
+        btnChat = view.findViewById(R.id.btnChat);
+        txtSearch = view.findViewById(R.id.txtSearch);
+        btnSearch = view.findViewById(R.id.btnSearch);
+        btnSell = view.findViewById(R.id.btnSell);
+
+
         return view;
     }
 }
