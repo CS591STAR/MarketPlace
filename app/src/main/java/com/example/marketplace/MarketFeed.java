af9af6db12a7dc6593cf1e573dbe768b0c1ffe36
mirtim <mirtim@bu.edu>
2019-11-03 22:28:10 -0500

    fragment not loading

--- app/src/main/java/com/example/marketplace/MarketFeed.java
+++ app/src/main/java/com/example/marketplace/MarketFeed.java
@@ -1,14 +1,22 @@
 package com.example.marketplace;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MarketFeed extends AppCompatActivity {
 
+    private Button btnTest;
+    private TextView txtTest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feed_layout);
+
+        btnTest = findViewById(R.id.btnTest);
+        txtTest = findViewById(R.id.txtTest);
     }
 }
--- app/src/main/java/com/example/marketplace/NavBarFragment.java
+++ app/src/main/java/com/example/marketplace/NavBarFragment.java
@@ -22,6 +22,11 @@ public class NavBarFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_nav_bar, container, false);
