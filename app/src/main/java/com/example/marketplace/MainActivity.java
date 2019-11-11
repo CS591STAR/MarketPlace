8a15dfd1d35f221808a5c8023856c91b52025849
Simon Acevedo Rodriguez <sacevedo@bu.edu>
2019-11-10 19:39:41 -0500

    zipcode api fetch
    fetching distance between two zipcodes

--- app/src/main/java/com/example/marketplace/MainActivity.java
+++ app/src/main/java/com/example/marketplace/MainActivity.java
@@ -12,17 +12,29 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnLogin;
 
+    // This button is entirely for test purposes only; will be removed later
+    private Button checkZipCodeAPI;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnLogin = findViewById(R.id.btnLogin);
+        checkZipCodeAPI = findViewById(R.id.openZipCodeTest);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), MarketFeed.class);
+                startActivity(intent);
+            }
+        });
+
+        checkZipCodeAPI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ZipCodeAPI.class);
                 startActivity(intent);
             }
         });
