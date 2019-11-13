92eb3ac23b8f5df0c4f852ea3520bf82235a7f64
Simon Acevedo Rodriguez <sacevedo@bu.edu>
2019-11-12 19:50:42 -0500

    add comments to zipcodeAPI class

--- app/src/main/java/com/example/marketplace/ZipCodeAPI.java
+++ app/src/main/java/com/example/marketplace/ZipCodeAPI.java
@@ -29,11 +29,17 @@ public class ZipCodeAPI  extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.zipcodeapi);
 
+        // For the purpose of testing the api get call, we are going to add
+        // a simple layout that allows users to enter two random zip codes and when they click
+        // on the button that says 'Check Distance` the output of the fetch call will be placed
+        // in the 'Result' text view.
+
         zipcode1 = findViewById(R.id.zipcode1);
         zipcode2 = findViewById(R.id.zipcode2);
         checkDisBtn = findViewById(R.id.checkDisBtn);
         resultTxt = findViewById(R.id.resultTxt);
 
+        // Call the getDistance() method that makes the get call to the api
         checkDisBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,12 +52,19 @@ public class ZipCodeAPI  extends AppCompatActivity {
         });
     }
 
-    // The following method is used to make the API call from the zipcode api
+    // The following method is used to make the API call
 
     public void getDistance() throws IOException {
+
+        // The following concatenates the zip codes that the user entered
+        // with the rest of the link needed to make the get call
+
         String dist1 = zipcode1.getText().toString();
         String dist2 = zipcode2.getText().toString();
         String locations = dist1 + "/" + dist2 + "/mile";
+
+        // We use OkHttpClient to make the get call with the headers needed as stated in the
+        // api documentation
 
         OkHttpClient client = new OkHttpClient();
         String url = "https://redline-redline-zipcode.p.rapidapi.com/rest/distance.json/" + locations;
@@ -63,13 +76,14 @@ public class ZipCodeAPI  extends AppCompatActivity {
                 .addHeader("x-rapidapi-key",gitignore.zipcodeAPIKey)
                 .build();
 
-
+        // the following methods were required by the newCall and callBack interfaces
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
                 e.printStackTrace();
             }
 
+            // Here we set the 'Response' text view to the get call response
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 if (response.isSuccessful()) {
