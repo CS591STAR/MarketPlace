14376f93f36172879bde7355f199bdd02d24cbab
RayRedeemer <supercontra@outlook.com>
2019-11-20 05:23:17 +0800

    update chatroom button

--- app/src/main/java/com/example/marketplace/Profile.java
+++ app/src/main/java/com/example/marketplace/Profile.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -38,6 +40,14 @@ public class Profile extends AppCompatActivity {
         btnPosts = findViewById(R.id.btnPosts);
         btnChats = findViewById(R.id.btnChats);
         txtUni = findViewById(R.id.txtUni);
+
+        //add chat button listener
+        btnChats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getBaseContext(), Chatroom.class));
+            }
+        });
 
     }
 }
