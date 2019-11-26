3c5b4675fba4c2a7b4bb729cf0da34e07f2c5b6c
RayRedeemer <supercontra@outlook.com>
2019-11-27 04:58:37 +0800

    minor change

--- app/src/main/java/com/example/marketplace/MarketFeed.java
+++ app/src/main/java/com/example/marketplace/MarketFeed.java
@@ -39,9 +39,6 @@ public class MarketFeed extends Fragment {
         MFL = (MarketFeedListener) context;
     }
 
-    User you;
-    SharedPreferences sharedPref;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
